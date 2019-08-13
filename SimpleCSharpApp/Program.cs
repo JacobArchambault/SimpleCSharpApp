d8264ff817b72243a94052ba92f2dcf1acf6142a
Jacob Archambault <jacob.archambault@waystar.com>
2019-08-13 12:07:12 -0400

    Add print environment info

--- SimpleCSharpApp/Program.cs
+++ SimpleCSharpApp/Program.cs
@@ -8,12 +8,26 @@ namespace SimpleCSharpApp
 {
     class Program
     {
+        static void ShowEnvironmentDetails()
+        {
+            // Print out teh drives on this machine,
+            // and other interesting details.
+            foreach (string drive in Environment.GetLogicalDrives())
+                Console.WriteLine("Drive: {0}", drive);
+
+            Console.WriteLine("OS: {0}", Environment.OSVersion);
+            Console.WriteLine("Number of processors: {0}", Environment.ProcessorCount);
+            Console.WriteLine(".NET version: {0}", Environment.Version);
+        }
         static int Main(string[] args)
         {
             // Display a simple message to the user.
             Console.WriteLine("***** My First C# App *****");
             Console.WriteLine("Hello World!");
             Console.WriteLine();
+
+            // Helper method within Program class.
+            ShowEnvironmentDetails();
 
             // Wait for Enter key to be pressed before shutting down.
             Console.ReadLine();
